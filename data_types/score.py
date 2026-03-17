d34880551f9a9124fc3d7528c6b6fae48c8269c8
CarlosArguelloDev <carlos.arguello.dev@gmail.com>
2026-03-16 19:42:37 -0600

    fet: add solution to find the runner-up score

--- data_types/score.py
+++ data_types/score.py
@@ -1,6 +1,14 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-arr = list(set(arr))
-arr.remove(max(arr))
-print(max(arr))
+max = arr[0]
+runnerUp = 0
+for i in range(len(arr)):
+    if arr[i] > max:
+        runnerUp = max
+        max = arr[i]
+    elif arr[i] > runnerUp and arr[i] != max:
+        runnerUp = arr[i]
+
+
+print(runnerUp)

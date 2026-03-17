5cd4dc7055f729d3c3bb6678324e338e787e46aa
CarlosArguelloDev <carlos.arguello.dev@gmail.com>
2026-03-17 13:15:38 -0600

    refactor: add -inf to initial values

--- data_types/score.py
+++ data_types/score.py
@@ -1,14 +1,12 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-max = arr[0]
-runnerUp = 0
+max = runnerUp = float('-inf')
 for i in range(len(arr)):
     if arr[i] > max:
         runnerUp = max
         max = arr[i]
     elif arr[i] > runnerUp and arr[i] != max:
         runnerUp = arr[i]
-
 
 print(runnerUp)
